68650ddd8990459cc6f8b1300b05cc790d4d51e5
leedohyeon0502 <leedohyeono0502@gmail.com>
2022-12-14 15:56:58 +0900

    Column align

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            body: Row(
+            body: Column(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: const [Icon(Icons.star), Icon(Icons.star), Icon(Icons.star)],
     )));
