9e87efcba2cc98c6321fe5cf84190e6f51223fb7
leedohyeon0502 <leedohyeono0502@gmail.com>
2022-12-15 12:39:43 +0900

    for use

--- lib/main.dart
+++ lib/main.dart
@@ -7,44 +7,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  static List<Widget> users = [];
   @override
   Widget build(BuildContext context) {
+    for (int i = 0; i < 10; i++) {
+      users.add(Row(children: [Icon(Icons.account_circle), Text('data')]));
+    }
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            appBar: AppBar(
-                iconTheme: IconThemeData(color: Colors.black),
-                title: Text(
-                  '금호동 3가',
-                  style: TextStyle(fontWeight: FontWeight.w700),
-                ),
-                actions: [
-                  Icon(Icons.search),
-                  Icon(Icons.list),
-                  Icon(Icons.notifications),
-                ]),
-            body: Container(
-              height: 150,
-              padding: EdgeInsets.all(10),
-              child: Row(
-                children: [
-                  Image.asset('assets/camera.png', width: 150),
-                  Container(
-                    width: 300,
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('카메라팝니다'),
-                          Text('금호동 3가'),
-                          Text('7000원'),
-                          Text('아이콘이랑 글자'),
-                          Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [Icon(Icons.favorite), Text('4')])
-                        ]),
-                  )
-                ],
-              ),
-            )));
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(),
+        body: ListView(
+          children: users,
+        ),
+      ),
+    );
   }
 }
