27e0136ecb2ce64d16d94f1e3c39470d9a4a75a6
leedohyeon0502 <leedohyeono0502@gmail.com>
2022-12-15 12:01:57 +0900

    design

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
-            appBar: AppBar(title: Text('앱임')),
+            appBar: AppBar(
+                iconTheme: IconThemeData(color: Colors.black),
+                title: Text(
+                  '금호동 3가',
+                  style: TextStyle(fontWeight: FontWeight.w700),
+                ),
+                actions: [
+                  Icon(Icons.search),
+                  Icon(Icons.list),
+                  Icon(Icons.notifications),
+                ]),
             body: Container(
-              width: 50,
-              height: 50,
-              decoration:
-                  BoxDecoration(border: Border.all(color: Colors.black)),
+              height: 150,
+              padding: EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  Image.asset('assets/camera.png', width: 150),
+                  Container(
+                    width: 300,
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('카메라팝니다'),
+                          Text('금호동 3가'),
+                          Text('7000원'),
+                          Text('아이콘이랑 글자'),
+                          Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [Icon(Icons.favorite), Text('4')])
+                        ]),
+                  )
+                ],
+              ),
             )));
   }
 }
